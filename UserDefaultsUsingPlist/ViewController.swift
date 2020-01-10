9596b9ae82bc3e7221c0376e230ad3d063c8feab
nagaraju <naga.thota99@gmail.com>
2020-01-10 10:59:48 +0530

    Initial Commit

---
+++ UserDefaultsUsingPlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsUsingPlist
+//
+//  Created by THOTA NAGARAJU on 1/10/20.
+//  Copyright © 2020 THOTA NAGARAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
